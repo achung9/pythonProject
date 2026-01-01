d8116058fd63caa407eac23c039d953cdf4d430b
Pham Anh Chung <anhchung856@gmail.com>
2026-01-01 16:11:42 +0700

    fix bug

--- model/Library.py
+++ model/Library.py
@@ -3,3 +3,15 @@ class Library:
         self.books = []
     def addBook(self, book):
         self.books.append(book)
+    def searchBook(self, title):
+        for book in self.books:
+            if book.title == title:
+                return True
+        return False
+    def printBooks(self):
+        for book in self.books:
+            print(book)
+    def searchBooksbyAuthor(self, author):
+        for book in self.books:
+            if book.author == author:
+                print(book)
